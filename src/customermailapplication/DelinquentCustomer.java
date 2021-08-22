f1cb1cd66fb3abd34d7ccdebf5ce9344d5dcef68
Peeraya123 <prayuthovantin@gmail.com>
2021-08-22 17:48:42 +0700

    First Version

---
+++ src/customermailapplication/DelinquentCustomer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package customermailapplication;
+
+/**
+ *
+ * @author peeraya
+ */
+// make this class a subclass of customer
+public class DelinquentCustomer extends Customer {
+
+    // Complete this class
+    @Override
+    public String createMail() { // implement return ค่า Delinquent Customer
+        return "Delinquent Customer";
+    }
+
+}
